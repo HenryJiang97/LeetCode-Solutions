d834008ef1777852ac7335aad7ea22d158816645
Henry Jiang <jianghanhua97@hotmail.com>
2019-03-31 22:07:24 -0400

    Version 03-31-19

--- ex390.java
+++ ex390.java
@@ -2,18 +2,23 @@ import java.util.*;
 
 class Solution {
     public int lastRemaining(int n) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= n; i++)    list.add(i);
+        int head = 1;
+        boolean fromLeft = true;
+        int remaining = n;
+        int gap = 1;
         
-        while (list.size() > 1) {
-            int i = 0;
-            while (i < list.size()) {
-                list.remove(i);
-                i += 2;
-            }
-            Collections.reverse(list);
+        while (remaining > 1) {
+            // If start from left or the from right but the remaining number is odd,
+            // Move head to the next gap position
+            if (fromLeft || remaining % 2 == 1)
+                head += gap;
+            
+            // Update gap, remaining and the state of fromLeft
+            gap *= 2;
+            remaining /= 2;
+            fromLeft = !fromLeft;
         }
         
-        return list.get(0);
+        return head;
     }
 }
