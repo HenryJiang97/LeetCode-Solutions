ea615731570df0c5add4a93c6105771a0ca6cab2
Henry Jiang <jianghanhua97@hotmail.com>
2019-04-06 22:52:45 -0400

    Version 04-06-19

---
+++ ex403.java
@@ -0,0 +1,35 @@
+// Hash Table DP
+// Build a dp map to store the possible k at each stone
+
+import java.util.*;
+
+class Solution {
+    public boolean canCross(int[] stones) {
+        // Corner case
+        if (stones.length == 0)    return false;
+        if (stones.length == 1)    return true;
+        
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        
+        for (int i = 0; i < stones.length; i++) {
+            map.put(stones[i], new HashSet<Integer>());
+        }
+        if (map.containsKey(stones[0] + 1))
+            map.get(stones[0] + 1).add(1);
+        else
+            return false;
+        
+        
+        for (int i = 1; i < stones.length; i++) {
+            Set<Integer> set = map.get(stones[i]);
+
+            for (int k : set) {
+                if (map.containsKey(stones[i] + k - 1) && k - 1 != 0)    map.get(stones[i] + k - 1).add(k - 1);
+                if (map.containsKey(stones[i] + k))    map.get(stones[i] + k).add(k);
+                if (map.containsKey(stones[i] + k + 1))    map.get(stones[i] + k + 1).add(k + 1);
+            }
+        }     
+        
+        return map.get(stones[stones.length - 1]).size() == 0 ? false : true;
+    }
+}
---
+++ ex404.java
@@ -0,0 +1,29 @@
+\/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null))    return 0;
+        return pre_order(root, null, 0);
+    }
+    
+    public int pre_order(TreeNode root, TreeNode lastRoot, int sum) {
+        if (root.left != null)
+            sum = pre_order(root.left, root, sum);
+        
+        if (root.right != null)
+            sum = pre_order(root.right, root, sum);
+        
+        if (root.left == null && root.right == null && lastRoot.left == root) {
+            sum += root.val;
+        }
+       
+        return sum;
+    }
+}
