4868b43eedb12d9b42550a2e64d97bc12169942d
Henry Jiang <jianghanhua97@hotmail.com>
2019-07-27 12:21:08 +0800

    Updated ex316

--- code/ex316.cpp
+++ code/ex316.cpp
@@ -1,4 +1,4 @@
-// Monotonous stack
+// Monotone stack
 
 class Solution {
 public:
@@ -9,13 +9,14 @@ public:
         vector<int> last_idx(26);
         for (int i = 0; i < n; i++)    last_idx[s[i] - 'a'] = i;
         
-        // Use a monotonous stack to store the result
+        // Use a monotone stack to store the result
         stack<char> stk;    stk.push(s[0]);
-        unordered_set<char> set;    set.insert(s[0]);
+        unordered_set<char> set;    set.insert(s[0]);    // Keep track of if this char appeared before
         
         for (int i = 1; i < n; i++) {
-            if (set.find(s[i]) != set.end())    continue;
+            if (set.find(s[i]) != set.end())    continue;    // Skip this char if it already existed before
             
+            // Remove chars before i which are lexicographically larger than s[i] to keep monotony of the stack
             while (!stk.empty() && stk.top() >= s[i] && last_idx[stk.top() - 'a'] >= i) {
                 set.erase(stk.top());
                 stk.pop();
@@ -25,6 +26,7 @@ public:
             set.insert(s[i]);
         }
         
+        // Save to result
         string res = "";
         while (!stk.empty()) {
             res = stk.top() + res;
