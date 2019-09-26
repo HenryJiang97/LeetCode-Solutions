bcb1a2cae68986c2644652ecb0dd373ca659ce82
Henry Jiang <jianghanhua97@hotmail.com>
2019-09-26 11:52:05 -0700

    Updated ex355.java

--- code/ex355.java
+++ code/ex355.java
@@ -1,83 +1,72 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-
 class Twitter {
-    // Create a data structure: tweet
-    class Tweet {
-        int userId;
-        int tweetId;
-        public Tweet(int user, int tweet) {userId = user;    tweetId = tweet;}
-    }
-    
-    // Global variable
-    public List<Tweet> tweet;
-    public HashMap<Integer, List<Integer>> follows;
-    
+    public int time;
+    public Map<Integer, List<Integer>> tweets;
+    public Map<Integer, Integer> id2time;
+    public Map<Integer, Set<Integer>> follow;
 
     /** Initialize your data structure here. */
     public Twitter() {
-        tweet = new ArrayList<Tweet>();
-        follows = new HashMap<Integer, List<Integer>>();
+        time = 0;
+        tweets = new HashMap<Integer, List<Integer>>();
+        id2time = new HashMap<Integer, Integer>();
+        follow = new HashMap<Integer, Set<Integer>>();
     }
     
-
     /** Compose a new tweet. */
     public void postTweet(int userId, int tweetId) {
-        // Add user to follows map
-        if (!follows.containsKey(userId))    follows.put(userId, new ArrayList<Integer>());
+        id2time.put(tweetId, time++);
         
-        // Add tweet to tweet list
-        tweet.add(new Tweet(userId, tweetId));
+        List<Integer> list = tweets.getOrDefault(userId, new ArrayList<Integer>());
+        list.add(tweetId);
+        tweets.put(userId, list);
     }
     
-
     /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
     public List<Integer> getNewsFeed(int userId) {
-        // If userid not in follows map
-        if (!follows.containsKey(userId))    return new ArrayList<>();
-
         List<Integer> feed = new ArrayList<>();
-        List<Integer> feedUser = follows.get(userId);
-        feedUser.add(userId);    // Add the user itself to the list
-
-        int counter = 0;
-        for (int i = tweet.size() - 1; i >= 0; i--) {
-            if (feedUser.contains(tweet.get(i).userId)) {
-                if (counter >= 10)    break;
-                feed.add(tweet.get(i).tweetId);
-                counter++;
+        // Add his personal tweets to the feed
+        if (tweets.containsKey(userId)) {
+            for (int tweet : tweets.get(userId))
+                feed.add(tweet);
+        }
+        
+        // Add the tweets from people he follows
+        if (follow.containsKey(userId)) {
+            System.out.println(follow.get(userId));
+            for (int f : follow.get(userId)) {
+                if (tweets.containsKey(f)) {
+                    for (int tweet : tweets.get(f)) {
+                        feed.add(tweet);
+                    }
+                } 
             }
         }
-
-        return feed;
+        
+        Collections.sort(feed, new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return id2time.get(b) - id2time.get(a);
+            }
+        });
+        
+        return feed.subList(0, Math.min(feed.size(), 10));
     }
     
-
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
     public void follow(int followerId, int followeeId) {
-        // Add user to follows map if not exist
-        if (!follows.containsKey(followerId))    follows.put(followerId, new ArrayList<Integer>());
-        
-        List<Integer> f = follows.get(followerId);
-        f.add(followeeId);
-        follows.put(followerId, f);
+        if (followerId == followeeId)    return;
+        Set<Integer> followees = follow.getOrDefault(followerId, new HashSet<Integer>());
+        followees.add(followeeId);
+        follow.put(followerId, followees);
     }
     
-
     /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
     public void unfollow(int followerId, int followeeId) {
-        if (followerId != followeeId) {
-            if(follows.containsKey(followerId)) {
-                List<Integer> f = follows.get(followerId);
-                
-                if (f.contains(followeeId)) {
-                    f.remove(f.indexOf(followeeId));
-                    follows.put(followerId, f);
-                }
-            }        
+        if (!follow.containsKey(followerId) || followerId == followeeId)    return;
+        Set<Integer> followees = follow.get(followerId);
+        if (followees.contains(followeeId)) {
+            followees.remove(followeeId);
         }
+        follow.put(followerId, followees);
     }
 }
 
