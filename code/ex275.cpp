e7da5a01cc3aeeb9ecc070fb221a8209396ee298
Henry Jiang <jianghanhua97@hotmail.com>
2019-10-21 23:23:18 -0700

    Updated ex373.cpp

---
+++ code/ex275.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        
+        int lo = 0, hi = n - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (citations[mid] == n - mid)
+                return citations[mid];
+            else if (citations[mid] < n - mid)
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+        
+        return n - lo;
+    }
+};
--- code/ex373.cpp
+++ code/ex373.cpp
@@ -1,30 +1,23 @@
-// Heap
-
-struct cmp {
-    bool operator() (vector<int>& a, vector<int>& b) {
-        return a[0] > b[0];
-    }
-};
-
 class Solution {
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int m = nums1.size(), n = nums2.size();
-        if (m == 0 || n == 0)    return {};
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int K) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        if (n1 == 0 || n2 == 0)    return {};
+        
         vector<vector<int>> res;
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        pq.push({nums1[0] + nums2[0], 0, 0});
+        int k = 0;
         
-        priority_queue<vector<int>, vector<vector<int>>, cmp> pq;
-        pq.push(vector<int>{nums1[0] + nums2[0], 0, 0});
-        
-        while (k > 0 && !pq.empty()) {
-            vector<int> out = pq.top();
-            pq.pop();
-            int i = out[1], j = out[2];
-            res.push_back({nums1[i], nums2[j]});
-            k--;
+        while (!pq.empty()) {
+            auto out = pq.top();    pq.pop();
+            int p1 = out[1], p2 = out[2];
+            res.push_back({nums1[p1], nums2[p2]});
+            if (++k == K)    return res;
             
-            if (j + 1 < n)    pq.push({nums1[i] + nums2[j + 1], i, j + 1});
-            if (j == 0 && i + 1 < m)    pq.push({nums1[i + 1] + nums2[j], i + 1, j});
+            // Push next larger pairs to the pq
+            if (p1 + 1 < n1)    pq.push({nums1[p1 + 1] + nums2[p2], p1 + 1, p2});
+            if (p1 == 0 && p2 + 1 < n2)    pq.push({nums1[p1] + nums2[p2 + 1], p1, p2 + 1});
         }
         
         return res;
