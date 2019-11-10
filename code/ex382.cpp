b62f58170f1ccb3ef528d4449e818b6cc537b4bd
Henry Jiang <jianghanhua97@hotmail.com>
2019-11-10 01:44:30 -0800

    Added ex382.cpp

---
+++ code/ex382.cpp
@@ -0,0 +1,38 @@
+// Reservoir sampling
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    ListNode* head;
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        ListNode* h = head;
+        int k = 0, x = 0;
+        while (h != NULL) {
+            int mod = rand() % (++k);
+            if (mod == 0)
+                x = h->val;
+            h = h->next;
+        }
+        return x;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
