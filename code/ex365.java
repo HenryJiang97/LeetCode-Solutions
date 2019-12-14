067f6bf503a71b046bcfa0d4ec13c97bbff862f7
Henry Jiang <jianghanhua97@hotmail.com>
2019-12-14 13:23:01 -0800

    Updated ex365.java

--- code/ex365.java
+++ code/ex365.java
@@ -1,15 +1,11 @@
-class Solution {    
+class Solution {
     public boolean canMeasureWater(int x, int y, int z) {
-        if (x + y == z)    return true;
-        if (x + y < z)    return false;     
-        int gcp = GCP(x, y);
-
-        return (x != 0 && z % x == 0) || (y != 0 && z % y == 0) || (gcp != 0 && z % gcp == 0);
+        if (z > x + y || z < 0)    return false;
+        return (z == 0 || z % gcd(x, y) == 0);
     }
     
-    // Get the greatest common divisor
-    public int GCP(int x, int y) {
-        if (x == 0)    return y;
-        return GCP(y % x, x);
+    private int gcd(int a, int b) {
+        if (b == 0)    return a;
+        return gcd(b, a % b);
     }
 }
