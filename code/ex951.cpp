503fd40211b9ad4007dcc5a66837722fe0d862e2
Henry Jiang <jianghanhua97@hotmail.com>
2020-01-02 22:22:08 -0800

    Added ex951.cpp

---
+++ code/ex951.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if (root1 == NULL && root2 == NULL)    return 1;
+        if (root1 == NULL && root2 != NULL || root1 != NULL && root2 == NULL)    return 0;
+        if (root1->val != root2->val)    return 0;
+        return flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right) || flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left);
+    }
+};
