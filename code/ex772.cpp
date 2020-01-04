e42a14fdb23608ef9dd5bd09e5594bf2d9d39ae8
Henry Jiang <jianghanhua97@hotmail.com>
2020-01-03 17:19:20 -0800

    Added ex772.cpp

---
+++ code/ex772.cpp
@@ -0,0 +1,54 @@
+typedef long long ll;
+
+class Solution {
+public:
+    int calculate(string s) {
+        queue<char> que;
+        for (char c : s)
+            if (c != ' ')    que.push(c);
+        if (que.empty())    return 0;
+        que.push('e');    // End sign
+        return cal(que);
+    }
+    
+private:
+    int cal(queue<char>& que) {
+        char sign = '+';
+        ll num = 0;
+        stack<ll> stk;
+        
+        while (!que.empty()) {
+            char c = que.front();    que.pop();
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            else if (c == '(') {
+                num = cal(que);
+            }
+            else {    // When meets operators or ')', get the result of last part
+                if (sign == '+')
+                    stk.push(num);
+                else if (sign == '-')
+                    stk.push(-num);
+                else if (sign == '*') {
+                    int last = stk.top();    stk.pop();
+                    stk.push(last * num);
+                } else if (sign == '/') {
+                    int last = stk.top();     stk.pop();;
+                    stk.push(last / num);
+                }
+                num = 0;
+                sign = c;
+                if (c == ')')    break;
+            }
+        }
+        
+        ll sum = 0;
+        while (!stk.empty()) {
+            sum += stk.top();
+            stk.pop();
+        }
+        
+        return (int)sum;
+    }
+};
