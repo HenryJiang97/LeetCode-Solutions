c5bb53ee48bf00ce2fcbfba0cb15770a957308c6
Henry Jiang <jianghanhua97@hotmail.com>
2020-01-16 15:36:21 -0800

    Added ex814.java

---
+++ code/ex814.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null)    return null;
+        TreeNode left = pruneTree(root.left);
+        TreeNode right = pruneTree(root.right);
+        root.left = left;    root.right = right;
+        if (root.val == 0 && left == null && right == null)    return null;
+        else    return root;
+    }
+}
