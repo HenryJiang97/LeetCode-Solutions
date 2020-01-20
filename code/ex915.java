adf75a30836c20082d48d727249a563b1b7daf67
Henry Jiang <jianghanhua97@hotmail.com>
2020-01-19 23:50:49 -0800

    Added ex915.java

--- code/ex915.java
+++ code/ex915.java
@@ -1,13 +1,13 @@
 class Solution {
     public int partitionDisjoint(int[] A) {
-        int n = A.length;
-        int[] left = new int[n], right = new int[n];
-        for (int i = 0; i < n; i++)
-            left[i] = i == 0 ? A[i] : Math.max(left[i - 1], A[i]);
-        for (int i = n - 1; i >= 0; i--)
-            right[i] = i == n - 1 ? A[i] : Math.min(right[i + 1], A[i]);
-        for (int i = 0; i < n - 1; i++)
-            if (left[i] <= right[i + 1])    return i + 1;
-        return 0;
+        int curMax = A[0], max = A[0], p = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] < curMax) {
+                p = i;
+                curMax = max;
+            }
+            max = Math.max(max, A[i]);
+        }
+        return p + 1;
     }
 }
