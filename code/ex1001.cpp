672baf92b34908155cf6c03ec3a05518e2960764
Henry Jiang <jianghanhua97@hotmail.com>
2020-01-24 11:48:18 -0800

    Updated ex1001.cpp

--- code/ex1001.cpp
+++ code/ex1001.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     vector<int> gridIllumination(int N, vector<vector<int>>& lamps, vector<vector<int>>& queries) {
         vector<int> res;
-        unordered_map<int, int> x, y, d1, d2;
-        unordered_map<int, unordered_set<int>> ls;
+        unordered_map<int, int> x, y, d1, d2;    // Hash table for row, col, two diagonals
+        unordered_map<int, unordered_set<int>> ls;    // Hash table for existing lamps
         
+        // Lit the lamps
         for (auto& l : lamps) {
             int i = l[0], j = l[1];
             if (ls.count(i) == 0 || ls[i].count(j) == 0) {    // If this lamp is not counted before
@@ -13,17 +14,22 @@ public:
             }
         }
         
+        // Process the query and dim the adjacent 8 lamps
         for (auto& query : queries) {
             int i = query[0], j = query[1];
-            if (x[i] || y[j] || d1[i + j] || d2[i - j]) {    // Lamp exists
+            if (x[i] > 0 || y[j] > 0 || d1[i + j] > 0 || d2[i - j] > 0) {    // Lamp exists
                 res.push_back(1);
                 // Remove from maps
-                for (auto li = i - 1; li <= i + 1; ++li)
+                for (auto li = i - 1; li <= i + 1; ++li) {
+                    if (li < 0 || li >= N)    continue;
                     for (auto lj = j - 1; lj <= j + 1; ++lj){
-                      if (ls[li].erase(lj)) {
-                        --x[li], --y[lj], --d1[li + lj], --d2[li - lj];
-                      }
+                        if (lj < 0 || lj >= N)    continue;
+                        if (ls.count(li) > 0 && ls[li].count(lj) > 0) {
+                            x[li]--;    y[lj]--;    d1[li + lj]--;    d2[li - lj]--;
+                            ls[li].erase(lj);
+                        }
                     }
+                }
             }
             else
                 res.push_back(0);
