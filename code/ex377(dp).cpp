fe9c2fdff431417725f8d20df22b75ea971e9eaa
Henry Jiang <jianghanhua97@hotmail.com>
2020-02-06 21:03:02 -0800

    Added ex377(dp).cpp

---
+++ code/ex377(dp).cpp
@@ -0,0 +1,15 @@
+// DP
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<unsigned long long> dp(target + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int n : nums) 
+                if (i - n >= 0)    dp[i] += dp[i - n];
+        }
+        return dp[target];
+    }
+};
