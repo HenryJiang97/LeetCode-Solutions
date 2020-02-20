62a02c97f86ac075936d1ee9b7812ff02fb8db3d
Henry Jiang <jianghanhua97@hotmail.com>
2020-02-20 15:31:39 -0800

    Updated ex221.cpp

--- code/ex221.cpp
+++ code/ex221.cpp
@@ -1,34 +1,17 @@
-// DP
-
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m = matrix.size();    if (m == 0)    return 0;
         int n = matrix[0].size();    if (n == 0)    return 0;
-        
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
         int MAX = 0;
-        // Init dp array
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++)
-            if (matrix[i][0] == '1') {
-                dp[i][0] = 1;
-                MAX = 1;
-            }
-            
-        for (int j = 0; j < n; j++)
-            if (matrix[0][j] == '1') {
-                dp[0][j] = 1;
-                MAX = 1;
-            }
-        
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] != '1')    continue;   
-                dp[i][j] = min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i - 1][j - 1] == '0')    continue;
+                dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
                 MAX = max(MAX, dp[i][j]);
             }
         }
-        
         return MAX * MAX;
     }
 };
