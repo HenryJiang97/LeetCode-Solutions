b2cecb1e6c5684dd9876f1d2983df4cf6e92cba8
Henry Jiang <jianghanhua97@hotmail.com>
2020-03-03 12:23:40 -0800

    Updated ex139.cpp

--- code/ex139.cpp
+++ code/ex139.cpp
@@ -1,23 +1,17 @@
-// DP
-
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.length();
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
         
-        unordered_set<string> set;
-        for (string word : wordDict)    set.insert(word);
-        
-        // Init dp array
         vector<bool> dp(n + 1);
-        dp[0] = true;
-        
-        // Build dp array
-        for (int i = 0; i < n; i++) {
-            if (!dp[i])    continue;
-            for (int j = i; j < n; j++) {
-                if (set.find(s.substr(i, j - i + 1)) != set.end())
-                    dp[j + 1] = true;
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
+                    dp[i] = 1;
+                    break;
+                }
             }
         }
         
