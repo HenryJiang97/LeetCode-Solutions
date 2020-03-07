429518172cdb06c8794d421b328647c87a085846
Henry Jiang <jianghanhua97@hotmail.com>
2020-03-06 23:26:06 -0800

    Added ex383.cpp

---
+++ code/ex383.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int dict[26] = {0};
+        for (char c : magazine)    dict[c - 'a']++;
+        for (char c : ransomNote)
+            if (--dict[c - 'a'] < 0)    return 0;
+        return 1;
+    }
+};
