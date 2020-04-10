575d8791dfb20cf87703f401f4cc5ae896b739ec
Henry Jiang <jianghanhua97@hotmail.com>
2020-04-10 00:13:56 -0700

    Added ex312.cpp

--- code/ex312.cpp
+++ code/ex312.cpp
@@ -1,29 +1,20 @@
 class Solution {
-    int n;
-    
 public:
     int maxCoins(vector<int>& nums) {
+        int n = nums.size();
         nums.insert(nums.begin(), 1);
-        nums.insert(nums.end(), 1);
-        n = nums.size();    if (n == 2)    return 0;
+        nums.push_back(1);
         
-        vector<vector<int>> memo(n, vector<int>(n));
-        return recurrsion(nums, 1, n - 2, memo);
-    }
-    
-private:
-    int recurrsion(vector<int>& nums, int lo, int hi, vector<vector<int>>& memo) {
-        if (lo > hi) {
-            return 0;
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2));
+        for (int l = 1; l <= n; l++) {
+            for (int i = 1; i <= n - l + 1; i++) {
+                int j = i + l - 1;
+                for (int k = i; k <= j; k++) {
+                    dp[i][j] = max(dp[i][j], dp[i][k - 1] + dp[k + 1][j] + nums[k] * nums[i - 1] * nums[j + 1]);
+                }
+            }
         }
         
-        if (memo[lo][hi] != 0)    return memo[lo][hi];
-        int score = 0;
-        for (int i = lo; i <= hi; i++) {
-            score = max(score, recurrsion(nums, lo, i - 1, memo) + recurrsion(nums, i + 1, hi, memo) + nums[lo - 1] * nums[i] * nums[hi + 1]);
-        }
-
-        memo[lo][hi] = score;
-        return score;
+        return dp[1][n];
     }
 };
