2504e87507a6ed928276c3233c22f455a3f71ddc
Henry Jiang <jianghanhua97@hotmail.com>
2020-04-23 18:10:02 -0700

    Updated ex91.cpp

--- code/ex91.cpp
+++ code/ex91.cpp
@@ -1,21 +1,19 @@
-// DP: dp[i] => dp[i + 1] + (substring of [i, i + 1] is between 1 and 26) ? dp[i + 2] : 0
-
 class Solution {
 public:
     int numDecodings(string s) {
-        // Corner cases
         int n = s.length();
-        if (n == 0)    return 0;
-        if (n == 1)    return s[0] == '0' ? 0 : 1;
         
-        // Build dp array
-        int dp[n + 1] = {0};    dp[n - 1] = s[n - 1] == '0' ? 0 : 1;    dp[n] = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (s[i] == '0')    {dp[i] = 0;    continue;}    // If the current char is '0', break string into two parts
-            dp[i] += dp[i + 1];
-            dp[i] += (stoi(s.substr(i, 2)) <= 26 && stoi(s.substr(i, 2)) >= 1) ? dp[i + 2] : 0;
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            // 1 digit code for the ending
+            if (s[i - 1] >= '1' && s[i - 1] <= '9')
+                dp[i] += dp[i - 1];
+            // 2 digits code for the ending
+            if (i - 2 >= 0 && (s[i - 2] == '1' || s[i - 2] == '2' && s[i - 1] <= '6'))
+                dp[i] += dp[i - 2];
         }
-
-        return dp[0];
+        
+        return dp[n];
     }
 };
