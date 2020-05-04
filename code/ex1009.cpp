1ce9a8d31542665a5f6152bbc2ec2b9fdd899bbd
Henry Jiang <jianghanhua97@hotmail.com>
2020-05-04 11:15:28 -0700

    Added ex1009.cpp

--- code/ex1009.cpp
+++ code/ex1009.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int findComplement(int num) {
+    int bitwiseComplement(int num) {
+        if (num == 0)    return 1;
         int res = 0, i = 0;
         while (num > 0) {
             int d = (num & 1 == 1) ? 0 : 1;
