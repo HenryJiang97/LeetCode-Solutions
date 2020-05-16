226fc31b452a2505771e5ac35362217314ce93f3
Henry Jiang <jianghanhua97@hotmail.com>
2020-05-15 18:38:21 -0700

    Added ex573.cpp

---
+++ code/ex573.cpp
@@ -0,0 +1,23 @@
+const int dir[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+class Solution {
+public:
+    int minDistance(int height, int width, vector<int>& tree, vector<int>& squirrel, vector<vector<int>>& nuts) {
+        int dis = 0;
+        
+        // Get the total distance between tree and every nuts
+        // Get the minimal distance to get every nuts
+        int minDiff = INT_MAX;
+        for (auto& nut : nuts) {
+            dis += getDis(nut, tree);
+            minDiff = min(minDiff, getDis(squirrel, nut) - getDis(nut, tree));
+        }
+        
+        return dis * 2 + minDiff;
+    }
+    
+private:
+    int getDis(vector<int>& p1, vector<int>& p2) {
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
+    }
+};
