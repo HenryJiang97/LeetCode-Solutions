4975e02a186796bbea74e2abcd21de3ddb1270c1
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-06-11 21:27:27 -0700

    Updated ex667.cpp

--- code/ex667.cpp
+++ code/ex667.cpp
@@ -4,8 +4,10 @@ public:
         vector<int> res;
         for (int lo = 1, hi = n; lo <= hi; ) {
             if (k > 0)
+                // Put lo and hi alternatively
                 res.push_back(k-- % 2 == 1 ? lo++ : hi--);
             else
+                // Add rest nums
                 res.push_back(lo++);
         }
         return res;
