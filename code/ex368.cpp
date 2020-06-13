685d1c99a3d4e06bcf621d7945b24f60a2c9daad
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-06-13 00:36:32 -0700

    Updated ex368.cpp

--- code/ex368.cpp
+++ code/ex368.cpp
@@ -4,26 +4,30 @@ public:
         int n = nums.size();    if (n == 0)    return {};
         sort(nums.begin(), nums.end());
         
-        int MAX = 1, end_idx = 0;
-        vector<int> dp(n, 1);
-        for (int i = 1; i < n; i++) {
+        int MAX = 1, end = 0;
+        vector<int> dp(n, 1), prev(n);
+        for (int i = 0; i < n; i++) {
+            prev[i] = i;
             for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0)
-                    dp[i] = max(dp[i], dp[j] + 1);
-                if (dp[i] > MAX) {
-                    MAX = dp[i];
-                    end_idx = i;
+                if (nums[i] % nums[j] == 0) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        prev[i] = j;
+                    }
                 }
+            }
+            if (dp[i] > MAX) {
+                MAX = dp[i];
+                end = i;
             }
         }
         
-        // Get result
+        // Get subset
         vector<int> res;
-        for (int i = end_idx, rest = MAX; i >= 0 && rest >= 0; i--) {
-            if (dp[i] == rest && nums[end_idx] % nums[i] == 0) {
-                res.push_back(nums[i]);
-                rest--;
-            }
+        while (1) {
+            res.push_back(nums[end]);
+            if (prev[end] == end)    break;
+            end = prev[end];
         }
         
         return res;
