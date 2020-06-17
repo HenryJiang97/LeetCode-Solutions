1f3f1874de8bfc1667f6258d2a0db1a3c9c9c7e9
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-06-17 00:20:44 -0700

    Updated ex130.cpp

--- code/ex130.cpp
+++ code/ex130.cpp
@@ -1,45 +1,41 @@
-// DFS
+const int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
 class Solution {
-    int m = 0, n = 0;
-    
+    int m, n;
 public:
     void solve(vector<vector<char>>& board) {
         m = board.size();    if (m == 0)    return;
         n = board[0].size();    if (n == 0)    return;
         
-        // To keep the char or change to X
-        auto keep = vector<vector<bool>>(m, vector<bool>(n));
-        
+        vector<vector<bool>> visited(m, vector<bool>(n));
         for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O' && !keep[i][0])    dfs(board, keep, i, 0);
-            if (board[i][n - 1] == 'O' && !keep[i][n - 1])    dfs(board, keep, i, n - 1);
-        }
-        
-        for (int i = 0; i < n; i++) {
-            if (board[0][i] == 'O' && !keep[0][i])    dfs(board, keep, 0, i);
-            if (board[m - 1][i] == 'O' && !keep[m - 1][i])    dfs(board, keep, m - 1, i);
-        }
-        
-        // Modify board
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; ++j) {
-                if (board[i][j] == 'O' && !keep[i][j])
-                    board[i][j] = 'X';
+            for (int j = 0; j < n; j++) {
+                if (i != 0 && j != 0 && i != m - 1 && j != n - 1)
+                    continue;
+                if (visited[i][j] || board[i][j] == 'X')    continue;
+                visited[i][j] = 1;
+                board[i][j] = 'K';
+                dfs(board, visited, i, j);
             }
         }
         
-        return;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'K')    board[i][j] = 'O';
+                else if (board[i][j] == 'O')    board[i][j] = 'X';
+            }
+        }
     }
     
 private:
-    void dfs(vector<vector<char>>& board, vector<vector<bool>>& keep, int i, int j) {
-        keep[i][j] = true;
-        
-        // Go four directions
-        if (i + 1 < m && board[i + 1][j] == 'O' && !keep[i + 1][j])    dfs(board, keep, i + 1, j);
-        if (i - 1 >= 0 && board[i - 1][j] == 'O' && !keep[i - 1][j])    dfs(board, keep, i - 1, j);
-        if (j + 1 < n && board[i][j + 1] == 'O' && !keep[i][j + 1])    dfs(board, keep, i, j + 1);
-        if (j - 1 >= 0 && board[i][j - 1] == 'O' && !keep[i][j - 1])    dfs(board, keep, i, j - 1);
+    void dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, int i, int j) {
+        for (auto d : dir) {
+            int ni = i + d[0], nj = j + d[1];
+            if (ni < 0 || nj < 0 || ni >= m || nj >= n || visited[ni][nj] || board[ni][nj] == 'X')
+                continue;
+            visited[ni][nj] = 1;
+            board[ni][nj] = 'K';
+            dfs(board, visited, ni, nj);
+        }
     }
 };
