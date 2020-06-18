e5d6fe2ac84c7c48077347538066eb0dd0eea2f0
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-06-18 00:32:47 -0700

    Updated ex275.cpp

--- code/ex275.cpp
+++ code/ex275.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n = citations.size();
+        int n = citations.size();    if (n == 0)    return 0;
         
         int lo = 0, hi = n - 1;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            
-            if (citations[mid] == n - mid)
-                return citations[mid];
-            else if (citations[mid] < n - mid)
-                lo = mid + 1;
-            else
-                hi = mid - 1;
+            if (n - mid == citations[mid])    return n - mid;
+            else if (n - mid > citations[mid])    lo = mid + 1;
+            else    hi = mid - 1;
         }
         
         return n - lo;
