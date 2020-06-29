7188cc7caa76b6b2ddcbda9de484c843b70052b6
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-06-29 13:35:18 -0700

    Added ex332.cpp

---
+++ code/ex332.cpp
@@ -0,0 +1,30 @@
+// Eulerian path
+//         -> D -> E
+// A -> B <-> F   
+
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        sort(tickets.begin(), tickets.end());
+        
+        unordered_map<string, queue<string>> map;
+        for (auto& ticket : tickets) {
+            map[ticket[0]].push(ticket[1]);
+        }
+        
+        vector<string> path;
+        dfs(map, path, "JFK");
+        reverse(path.begin(), path.end());
+        return path;
+    }
+    
+private:
+    void dfs(unordered_map<string, queue<string>>& map, vector<string>& path, string cur) {
+        while (!map[cur].empty()) {
+            string nxt = map[cur].front();
+            map[cur].pop();
+            dfs(map, path, nxt);
+        }
+        path.push_back(cur);
+    }
+};
