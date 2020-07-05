b31d649ee43c14b1d211ef4bc64e9732bfd21b8a
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-07-05 13:19:37 -0700

    Added ex63.cpp

--- code/ex63.cpp
+++ code/ex63.cpp
@@ -1,30 +1,20 @@
 class Solution {
-    int m, n;
-    vector<vector<int>> dir = {{1, 0}, {0, 1}};
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        m = grid.size();    if (m == 0)    return 0;
-        n = grid[0].size();    if (n == 0)    return 0;
+        int m = grid.size();    if (m == 0)    return 0;
+        int n = grid[0].size();    if (n == 0)    return 0;
+        if (grid[0][0] == 1 || grid[m - 1][n - 1] == 1)    return 0;
         
-        vector<vector<int>> memo(m, vector<int>(n, -1));
-        return dfs(grid, memo, 0, 0);
-    }
-    
-private:
-    int dfs(vector<vector<int>>& grid, vector<vector<int>>& memo, int i, int j) {
-        if (i == m - 1 && j == n - 1 && grid[m - 1][n - 1] == 0)    return 1;
-        if (grid[i][j] == 1)    return 0;
-        
-        if (memo[i][j] != -1)    return memo[i][j];
-        
-        int cnt = 0;
-        for (int d = 0; d < 2; d++) {
-            int ni = i + dir[d][0], nj = j + dir[d][1];
-            if (ni >= m || nj >= n)    continue;
-            cnt += dfs(grid, memo, ni, nj);
+        vector<vector<long>> dp(m, vector<long>(n));
+        dp[m - 1][n - 1] = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (grid[i][j] == 1)    continue;
+                if (i + 1 < m)    dp[i][j] = (dp[i][j] + dp[i + 1][j]);
+                if (j + 1 < n)    dp[i][j] = (dp[i][j] + dp[i][j + 1]);
+            }
         }
         
-        memo[i][j] = cnt;
-        return cnt;
+        return (int)dp[0][0];
     }
 };
