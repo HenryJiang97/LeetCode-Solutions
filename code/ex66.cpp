6a247210ead82106869356d4ccce23124e65026c
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-07-06 00:14:06 -0700

    Added ex66.cpp

---
+++ code/ex66.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        
+        int carry = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            int val = digits[i] + carry;
+            digits[i] = val % 10;
+            carry = val / 10;
+        }
+        if (carry == 1)    digits.insert(digits.begin(), 1);
+        
+        return digits;
+    }
+};
