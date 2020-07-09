26df4ceb8b5a2ee30a4ce3bba8ef22cddbcb3db5
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-07-09 00:42:23 -0700

    Updated ex662.cpp

--- code/ex662.cpp
+++ code/ex662.cpp
@@ -4,51 +4,33 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-typedef long long int64;
-
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         if (root == NULL)    return 0;
         
-        queue<TreeNode*> nodes;    nodes.push(root);
-        queue<int64> idx;    idx.push(1);
-        int64 MAX = 1;
+        unsigned long MAX = 0;
+        queue<pair<TreeNode*, unsigned long>> que;    // {Node, idx}
+        que.push({root, 1});
         
-        vector<TreeNode*> temp_nodes;
-        vector<int64> temp_idx;
-        while (!nodes.empty()) {
-            TreeNode* out_node = nodes.front();    nodes.pop();
-            int64 out_idx = idx.front();    idx.pop();
-            
-            if (out_node->left != NULL) {
-                temp_nodes.push_back(out_node->left);
-                temp_idx.push_back(out_idx * 2);
+        while (!que.empty()) {
+            int size = que.size();
+            unsigned long lo = -1, hi = -1;
+            for (int z = size; z > 0; z--) {
+                auto out = que.front();    que.pop();
+                if (z == size)    lo = out.second;
+                if (z == 1)    hi = out.second;
+                if (out.first->left != NULL)    que.push({out.first->left, out.second * 2});
+                if (out.first->right != NULL)    que.push({out.first->right, out.second * 2 + 1});
             }
-            if (out_node->right != NULL) {
-                temp_nodes.push_back(out_node->right);
-                temp_idx.push_back(out_idx * 2 + 1);
-            }
-            
-            if (nodes.empty()) {
-                // Copy elements from temp vectors back to queue
-                if (temp_nodes.size() != 0) {
-                    MAX = max(MAX, temp_idx[temp_idx.size() - 1] - temp_idx[0] + 1);
-                    
-                    for (auto& tn : temp_nodes)    nodes.push(tn);
-                    
-                    if (temp_idx.size() == 1)    idx.push(1);
-                    else    for (auto& ti : temp_idx)    idx.push(ti);
-                    
-                    temp_nodes.clear();    temp_idx.clear();
-                }   
-            }
+            MAX = max(MAX, hi - lo + 1);
         }
         
-        return MAX;
+        return (int)MAX;
     }
 };
