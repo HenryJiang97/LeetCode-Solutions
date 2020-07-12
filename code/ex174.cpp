07a1d04d91e33d2d871695793b3441e6ce299066
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-07-12 00:17:50 -0700

    Added ex190.cpp

--- code/ex174.cpp
+++ code/ex174.cpp
@@ -1,21 +1,14 @@
-const int dir[2][2] = {{-1, 0}, {0, -1}};
-
 class Solution {
 public:
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int m = dungeon.size();    if (m == 0)    return 0;
-        int n = dungeon[0].size();    if (n == 0)    return 0;
+        int m = dungeon.size(), n = dungeon[0].size();
         
-        vector<vector<int>> dp(m, vector<int>(n, INT_MAX / 2));
-        dp[m - 1][n - 1] = max(1, 1 - dungeon[m - 1][n - 1]);
-        
-        for (int i = m - 1; i >= 0; --i) {
-            for (int j = n - 1; j >= 0; --j) {
-                for (auto& d : dir) {
-                    int pi = i + d[0], pj = j + d[1];
-                    if (pi < 0 || pj < 0 || pi >= m || pj >= n)    continue;
-                    dp[pi][pj] = min(dp[pi][pj], max(1, dp[i][j] - dungeon[pi][pj]));
-                }
+        vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
+        dp[m - 1][n - 1] = dungeon[m - 1][n - 1] >= 1 ? 1 : 1 - dungeon[m - 1][n - 1];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i - 1 >= 0)    dp[i - 1][j] = min(dp[i - 1][j], max(dp[i][j] - dungeon[i - 1][j], 1));
+                if (j - 1 >= 0)    dp[i][j - 1] = min(dp[i][j - 1], max(dp[i][j] - dungeon[i][j - 1], 1));
             }
         }
         
--- code/ex190.cpp
+++ code/ex190.cpp
@@ -3,10 +3,8 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t res = 0;
         for (int i = 0; i < 32; i++) {
-            int bit = n & 1;
-            n = n >> 1;
-            res = res << 1;
-            res = res ^ bit;
+            res = (res << 1) + (n % 2 == 0 ? 0 : 1);
+            n >>= 1;
         }
         return res;
     }
