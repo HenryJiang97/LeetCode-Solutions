3aba147c4a7c7caf88c7148b748ed59fe71134ea
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-07-30 18:20:08 -0700

    Added ex1054.cpp

--- code/ex1054.cpp
+++ code/ex1054.cpp
@@ -1,46 +1,24 @@
-// Heap
-
 class Solution {
 public:
     vector<int> rearrangeBarcodes(vector<int>& barcodes) {
-        // Classify and get the info of these barcode
-        unordered_map<int, int> map;
-        for (auto barcode : barcodes) {
-            if (map.find(barcode) != map.end())
-                map[barcode]++;
-            else
-                map[barcode] = 1;
-        }
+        unordered_map<int, int> cnt;
+        for (int bar : barcodes)    cnt[bar]++;
         
         priority_queue<pair<int, int>> pq;
-        for (auto itr : map)
-            pq.push(make_pair(itr.second, itr.first));
+        for (auto& [k, v] : cnt)
+            pq.push({v, k});
         
-        vector<int> res;        
+        vector<int> res;
         while (!pq.empty()) {
-            if (pq.size() >= 2) {
-                vector<pair<int, int>> temp;
-                
-                // Add top two items in the queue to the result
-                res.push_back(pq.top().second);
-                temp.push_back(pq.top());
-                pq.pop();
-                
-                res.push_back(pq.top().second);
-                temp.push_back(pq.top());
-                pq.pop();
-                
-                // Add items in temp back to the queue
-                for (auto p : temp) {
-                    p.first -= 1;
-                    if (p.first != 0)    pq.push(p);
-                }
-            } else if (pq.size() == 1) {
-                if (pq.top().first > 1 || (res.size() > 0 && pq.top().second == res[res.size() - 1]))    return {};
-                else {
-                    res.push_back(pq.top().second);
-                    pq.pop();
-                }
+            pair<int, int> top = pq.top();    pq.pop();
+            if (res.size() > 0 && res.back() == top.second) {
+                pair<int, int> nxt = pq.top();    pq.pop();
+                res.push_back(nxt.second);
+                if (--nxt.first > 0)    pq.push(nxt);
+                pq.push(top);
+            } else {
+                res.push_back(top.second);
+                if (--top.first > 0)    pq.push(top);
             }
         }
         
