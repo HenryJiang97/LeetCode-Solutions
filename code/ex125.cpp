4c5920b148558a23dbeb597dcb062bfbeeaf68e0
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-08-03 00:17:21 -0700

    Updated ex125.cpp

--- code/ex125.cpp
+++ code/ex125.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n = s.length();
-        int lo = 0, hi = n - 1;
+        int lo = 0, hi = s.length() - 1;
         while (lo < hi) {
-            if (!isalpha(s[lo]) && !isdigit(s[lo]))    {lo++;    continue;}
-            if (!isalpha(s[hi]) && !isdigit(s[hi]))    {hi--;    continue;}
-            if (isdigit(s[lo]) && isdigit(s[hi]) && s[lo] == s[hi] ||
-               isalpha(s[lo]) && isalpha(s[hi]) && toupper(s[lo]) == toupper(s[hi])) {
-                lo++;    hi--;
-            } else {
-                return 0;
-            }
+            while (lo < hi && !isalpha(s[lo]) && !isdigit(s[lo]))    lo++;
+            while (lo < hi && !isalpha(s[hi]) && !isdigit(s[hi]))    hi--;
+            if (tolower(s[lo++]) != tolower(s[hi--]))    return 0;
         }
         return 1;
     }
