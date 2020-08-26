e8e36693fbb42240ca5fcb2942ebf4bf3d74a3fa
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-08-26 00:03:57 -0700

    Added ex412.cpp

--- code/ex128.cpp
+++ code/ex128.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if (nums.size() == 0)    return 0;
-        unordered_map<int, int> m;
-        for (int num : nums)    m[num] = 1;
-        int len = 1;
+        unordered_set<int> set(nums.begin(), nums.end());
+        int MAX = 0;
         for (int num : nums) {
-            int cur = num;
-            if (m.find(cur - 1) != m.end()) {
-                int cnt = 1;
-                while (m.find(cur - 1) != m.end() && m[cur - 1] == 1) {
-                    m.erase(cur - 1);
-                    cur--;
-                    cnt++;
+            int len = 0;
+            if (set.count(num - 1) == 0) {
+                while (set.count(num) > 0) {
+                    len++;    num++;
                 }
-                if (m.find(cur - 1) != m.end())    cnt += m[cur - 1];
-                m[num] = cnt;
-                len = max(len, cnt);
             }
+            MAX = max(MAX, len);
         }
-        return len;
+        return MAX;
     }
 };
---
+++ code/ex412.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res;
+        for (int i = 1; i <= n; i++)
+            res.push_back(i % 3 == 0 && i % 5 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : to_string(i));
+        return res;
+    }
+};
