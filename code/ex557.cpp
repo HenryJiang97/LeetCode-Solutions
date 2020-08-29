e1b79979c732a5cb4daaa1d085fe7d0e3ffe265f
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-08-29 11:56:29 -0700

    Updated ex557.cpp

--- code/ex557.cpp
+++ code/ex557.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.length();
-        
-        int lo = 0, hi = 0;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == ' ') {
-                rev(s, lo, hi);
-                lo = i + 1;
-            } else
-                hi = i;
+        string res = "", temp = "";
+        s += " ";
+        int i = 0;
+        for (char c : s) {
+            if (c == ' ') {
+                reverse(temp.begin(), temp.end());
+                if (i != 0)    res += " ";
+                res += temp;
+                temp = "";
+                i++;
+            } else {
+                temp += c;
+            }
         }
-        rev(s, lo, hi);
-        
-        return s;
-    }
-    
-private:
-    void rev(string &s, int lo, int hi) {
-        while (lo < hi) {
-            char temp = s[lo];    s[lo] = s[hi];    s[hi] = temp;
-            lo++;    hi--;
-        }
+        return res;
     }
 };
