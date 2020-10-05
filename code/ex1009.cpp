f6cc885875d162ea8745ccf9c277a9cb2fa565d1
Henry Jiang <henryjiang97@gmail.com>
2020-10-05 00:12:57 -0700

    Added ex1009.cpp

--- code/ex1009.cpp
+++ code/ex1009.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
-    int bitwiseComplement(int num) {
-        if (num == 0)    return 1;
-        int res = 0, i = 0;
-        while (num > 0) {
-            int d = (num & 1 == 1) ? 0 : 1;
-            res |= (d << (i++));
-            num >>= 1;
-        }
-        return res;
+    int bitwiseComplement(int N) {
+        if (N == 0)    return 1;
+        int d = 0;
+        while ((1 << d) <= N)    d++;
+        return N ^ ((1 << d) - 1);
     }
 };
