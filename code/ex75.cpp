b21b8c7a7c7c3853bef2548583137ca8209097a3
HenryJiang97 <jianghanhua97@hotmail.com>
2020-10-06 11:05:30 -0700

    Updated ex75.cpp

--- code/ex75.cpp
+++ code/ex75.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        
-        int lo = 0, hi = n - 1;
-        while (lo < hi && nums[lo] == 0)    lo++;
-        while (lo < hi && nums[hi] == 2)    hi--;
-        int i = lo;
-        while (i <= hi) {
-            if (nums[i] == 0)    {swap(nums[i], nums[lo++]);    i++;}
-            else if (nums[i] == 2)    swap(nums[i], nums[hi--]);
-            else    i++;
+        int p1 = 0, p2 = n - 1, p = 0;
+        while (p <= p2) {
+            if (nums[p] == 0) {
+                swap(nums[p++], nums[p1++]);
+            } else if (nums[p] == 2) {
+                swap(nums[p], nums[p2--]);
+            } else {
+                p++;
+            }
         }
     }
 };
