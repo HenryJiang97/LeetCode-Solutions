06b27c06647a1d924097ed2b309e2d87f43ba1f3
Henry Jiang <henryjiang97@gmail.com>
2020-11-08 11:35:11 -0800

    Added ex973.cpp

--- code/ex973.cpp
+++ code/ex973.cpp
@@ -1,24 +1,11 @@
-struct cmp {
-    bool operator()(vector<int>& a, vector<int>& b) {
-        return (a[0] * a[0] + a[1] * a[1]) < (b[0] * b[0] + b[1] * b[1]);
-    }
-};
-
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        priority_queue<vector<int>, vector<vector<int>>, cmp> pq;
-        for (auto& point : points) {
-            pq.push(point);
-            if (pq.size() > K)    pq.pop();
-        }
-        
-        vector<vector<int>> res;
-        while (!pq.empty()) {
-            res.push_back(pq.top());
-            pq.pop();
-        }
-        
-        return res;
+        sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b) {
+            double da = sqrt(a[0] * a[0] + a[1] * a[1]);
+            double db = sqrt(b[0] * b[0] + b[1] * b[1]);
+            return da < db;
+        });
+        return vector<vector<int>>(points.begin(), points.begin() + K);
     }
 };
