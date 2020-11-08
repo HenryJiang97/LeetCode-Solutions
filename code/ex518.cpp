fcd8c0c0e2f84c31618b88125d96d394ce67d55b
Henry Jiang <henryjiang97@gmail.com>
2020-11-07 21:10:22 -0800

    Added ex518.cpp

--- code/ex518.cpp
+++ code/ex518.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        
         vector<int> dp(amount + 1);
         dp[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= amount; j++) {
-                dp[j] += j - coins[i - 1] >= 0 ? dp[j - coins[i - 1]] : 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                dp[j] += j - coins[i] >= 0 ? dp[j - coins[i]] : 0;
             }
         }
-        
         return dp[amount];
     }
 };
