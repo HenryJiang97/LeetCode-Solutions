fb9ee9fe382f8d1c9d7d3c9a79a311fd3c86d1dd
Henry Jiang <henryjiang97@gmail.com>
2020-11-21 06:02:21 -0800

    Updated ex902.cpp

--- code/ex902.cpp
+++ code/ex902.cpp
@@ -1,33 +1,27 @@
-typedef long long ll;
-
 class Solution {
-    ll N;
-    int cnt = 0, n;
+    int cnt = 0;
 public:
-    int atMostNGivenDigitSet(vector<string>& D, int N) {
-        this->N = N;    this->n = to_string(N).size();    // Digit of N
-        if (N < stoll(D[0]))    return 0;
-        
-        for (int i = 1; i <= n - 1; i++)    // Count all possible numbers which has digit < n
-            cnt += pow(D.size(), i);
-        dfs(D, 0, 0);
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        string sn = to_string(n);
+        for (int i = 0; i < sn.length() - 1; i++) {    // Get count for number shorter than n
+            cnt += pow(digits.size(), i + 1);
+        }
+        dfs(digits, sn, "");
         return cnt;
     }
     
 private:
-    void dfs(vector<string>& D, ll num, int s) {
-        if (s == n) {
-            cnt++;
+    void dfs(vector<string>& digits, string sn, string cur) {
+        if (cur.length() == sn.length()) {
+            if (cur <= sn)    cnt++;
             return;
         }
         
-        for (int i = 0; i < D.size(); i++) {  
-            if ('0' + stoi(D[i]) < to_string(N)[s]) {    // Do it mathematically
-                cnt += pow(D.size(), n - s - 1);
-            }
-            else if ('0' + stoi(D[i]) == to_string(N)[s]) {
-                ll newNum = num * 10 + stoll(D[i]);
-                dfs(D, newNum, s + 1);
+        for (string d : digits) {
+            if (d[0] < sn[cur.length()]) {    // Current digit smaller than sn
+                cnt += pow(digits.size(), sn.length() - cur.length() - 1);
+            } else if (d[0] == sn[cur.length()]) {    // Current digit equal to sn
+                dfs(digits, sn, cur + d);
             }
         }
     }
