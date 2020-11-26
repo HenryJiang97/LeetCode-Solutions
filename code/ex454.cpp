7ef0d5a4fcfbde9e99b5945e0e1df2150c761295
Henry Jiang <henryjiang97@gmail.com>
2020-11-26 13:50:44 -0800

    Added ex454.cpp

---
+++ code/ex454.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int, int> ab;
+        for (int a : A) {
+            for (int b : B) {
+                ab[a + b]++;
+            }
+        }
+        
+        int cnt = 0;
+        for (int c : C) {
+            for (int d : D) {
+                int target = -c - d;
+                cnt += ab[target];
+            }
+        }
+        
+        return cnt;
+    }
+};
