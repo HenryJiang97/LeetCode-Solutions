3857618c57c35ec19494b6e6f3f1a31dae330350
Henry Jiang <henryjiang97@gmail.com>
2020-11-27 01:11:23 -0800

    Updated ex416.cpp

--- code/ex416.cpp
+++ code/ex416.cpp
@@ -7,25 +7,22 @@ public:
         
         // Get the target sum of each subset
         int sum = 0;
-        for (auto num : nums)    sum += num;
-        if (sum % 2 != 0)    return false;
-        int target = sum / 2;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % 2 == 1)    return 0;
+        sum /= 2;
         
         // Dynamic programming through the nums array to find out if an subset with sum of target exists
-        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
-        dp[0][0] = true;
-        
+        bool dp[n + 1][sum + 1];
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = 1;
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= target; j++) {
-                dp[i][j] = dp[i - 1][j];    // Not pick certain element, leave it for the second subset
-                
-                if (j >= nums[i - 1]) {
+            for (int j = 0; j <= sum; j++) {
+                dp[i][j] = dp[i][j] || dp[i - 1][j];
+                if (j - nums[i - 1] >= 0)
                     dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
-                }
             }
-            if (dp[i][target])    return true;
         }
         
-        return dp[n][target];
+        return dp[n][sum];
     } 
 };
