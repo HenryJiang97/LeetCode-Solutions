648265288e2229942e8f8ba63709468c74329ee0
Henry Jiang <henryjiang97@gmail.com>
2020-12-03 14:06:53 -0800

    Updated ex632.cpp

--- code/ex632(multimap).cpp
+++ code/ex632(multimap).cpp
@@ -3,30 +3,23 @@ public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
         int n = nums.size();
         
-        multimap<int, int> val2arr;    // Val -> array
-        vector<int> idx(n);    // arr -> idx
+        multimap<int, int> mmap;
+        vector<int> idx(n, 1);    // idx for each array
+        for (int i = 0; i < n; i++)    mmap.insert({nums[i][0], i});
         
-        for (int i = 0; i < n; i++)    val2arr.insert({nums[i][0], i});
-        
-        int MIN = INT_MAX;
-        vector<int> res{-1, -1};
+        vector<int> range{(*mmap.begin()).first, (*mmap.rbegin()).first};
         while (1) {
-            auto it = val2arr.begin();
-            auto a1 = *val2arr.begin(), a2 = *val2arr.rbegin();
-            if (a2.first - a1.first < MIN) {
-                MIN = a2.first - a1.first;
-                res[0] = a1.first, res[1] = a2.first;
-            }
+            int i = (*mmap.begin()).second, val = (*mmap.begin()).first;
+            if (idx[i] >= nums[i].size())    break;
             
-            if (idx[a1.second] + 1 >= nums[a1.second].size())
-                break;
-            else {
-                idx[a1.second]++;
-                val2arr.insert({nums[a1.second][idx[a1.second]], a1.second});
-                val2arr.erase(it);
+            mmap.erase(mmap.begin());
+            mmap.insert({nums[i][idx[i]++], i});
+            
+            if ((*mmap.rbegin()).first - (*mmap.begin()).first < range[1] - range[0]) {
+                range = {(*mmap.begin()).first, (*mmap.rbegin()).first};
             }
         }
         
-        return res;
+        return range;
     }
 };
