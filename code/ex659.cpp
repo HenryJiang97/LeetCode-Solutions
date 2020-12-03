2514403b4af418883e286c246689f50be2907a6c
Henry Jiang <henryjiang97@gmail.com>
2020-12-03 12:33:47 -0800

    Updated ex659.cpp

--- code/ex659.cpp
+++ code/ex659.cpp
@@ -4,19 +4,20 @@ public:
         unordered_map<int, int> cnt;
         for (int num : nums)    cnt[num]++;
         
-        unordered_map<int, int> remain;
+        unordered_map<int, int> ending;
         for (int num : nums) {
             if (cnt[num] == 0)    continue;
             cnt[num]--;
-            if (remain[num - 1] > 0) {
-                remain[num - 1]--;
-                remain[num]++;
+            if (ending[num - 1] > 0) {
+                ending[num - 1]--;
+                ending[num]++;
             } else if (cnt[num + 1] > 0 && cnt[num + 2] > 0) {
-                remain[num + 2]++;
                 cnt[num + 1]--;
                 cnt[num + 2]--;
-            } else 
+                ending[num + 2]++;
+            } else {
                 return 0;
+            }
         }
         
         return 1;
