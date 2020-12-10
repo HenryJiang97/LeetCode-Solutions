ca693bafe2f7c2782e64f4070e888d741d7af3d7
Henry Jiang <henryjiang97@gmail.com>
2020-12-10 14:16:02 -0800

    Updated ex96.cpp

--- code/ex996.cpp
+++ code/ex996.cpp
@@ -1,45 +1,44 @@
 class Solution {
-    unordered_map<int, int> cnt;
-    unordered_map<int, unordered_set<int>> squareful;
-    
+    int res = 0;
 public:
     int numSquarefulPerms(vector<int>& A) {
         int n = A.size();
-        
-        for (int a : A)    cnt[a]++;
-        
-        for (auto& e1 : cnt) {
-            for (auto& e2 : cnt) {
-                int i = e1.first, j = e2.first;
-                if ((int)sqrt(i + j) * (int)sqrt(i + j) == i + j) {
-                    squareful[i].insert(j);
+        sort(A.begin(), A.end());
+        vector<vector<int>> graph(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j)    continue;
+                if ((int)sqrt(A[i] + A[j]) * (int)sqrt(A[i] + A[j]) == A[i] + A[j]) {
+                    graph[i].push_back(j);
                 }
             }
         }
         
-        int res = 0;
-        for (auto e : cnt) {
-            cnt[e.first]--;
-            dfs(e.first, n - 1, res);
-            cnt[e.first]++;
+        // DFS
+        vector<bool> visited(n);
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && A[i] == A[i - 1])    continue;
+            visited[i] = 1;
+            dfs(A, graph, visited, i, 1);
+            visited[i] = 0;
         }
         
         return res;
     }
     
-private:
-    void dfs(int cur, int remain, int& res) {
-        if (remain == 0) {
+    void dfs(vector<int>& A, vector<vector<int>>& graph, vector<bool>& visited, int i, int len) {
+        if (len == A.size()) {
             res++;
             return;
         }
         
-        for (int nxt : squareful[cur]) {
-            if (cnt[nxt] > 0) {
-                cnt[nxt]--;
-                dfs(nxt, remain - 1, res);
-                cnt[nxt]++;
-            }
+        int prev = -1;
+        for (int nxt : graph[i]) {
+            if (visited[nxt] || A[nxt] == prev)    continue;
+            prev = A[nxt];
+            visited[nxt] = 1;
+            dfs(A, graph, visited, nxt, len + 1);
+            visited[nxt] = 0;
         }
     }
 };
