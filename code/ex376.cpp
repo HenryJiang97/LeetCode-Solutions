cc387b1ace37ed83c33844deaf95c07c19ae439c
Henry Jiang <henryjiang97@gmail.com>
2020-12-11 11:18:33 -0800

    Updated ex376.cpp

--- code/ex376.cpp
+++ code/ex376.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        if (n < 2)    return n;
-        
-        int increase = 1, decrease = 1;
+        if (n == 0)    return 0;
+        int up = 1, down = 1, MAX = 1;
         for (int i = 1; i < n; i++) {
-            int newIncrease = increase;
-            int newDecrease = decrease;
-            if (nums[i] > nums[i - 1]) {    // Increase
-                newIncrease = decrease + 1;
-            } else if (nums[i] < nums[i - 1]) {    // Decrease
-                newDecrease = increase + 1;
-            }
-            increase = newIncrease;
-            decrease = newDecrease;
+            int bkpUp = up, bkpDown = down;
+            if (nums[i] > nums[i - 1])
+                up = max(up, bkpDown + 1);
+            if (nums[i] < nums[i - 1])
+                down = max(down, bkpUp + 1);
+            MAX = max(MAX, max(up, down));
         }
-        
-        return max(increase, decrease);
+        return MAX;
     }
 };
