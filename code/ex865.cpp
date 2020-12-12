c752243983370c5e235b5e119194789a5cd840a8
Henry Jiang <henryjiang97@gmail.com>
2020-12-12 12:09:47 -0800

    Updated ex865.cpp

--- code/ex865.cpp
+++ code/ex865.cpp
@@ -4,53 +4,54 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        // Define the parent map to store the parent node of each node
-        unordered_map<TreeNode*, TreeNode*> parent;
-        parent[root] = root;
+        vector<TreeNode*> parent(501, NULL);
         
-        // DFS search to find the last layer
         queue<TreeNode*> que;
         que.push(root);
-        vector<TreeNode*> lastLayer;
-        while (!que.empty())
-        {
-            lastLayer.clear();
-            for (int z = que.size(); z > 0; --z)
-            {
-                auto out = que.front();    que.pop();
-                lastLayer.push_back(out);
-                
-                if (out->left != NULL)
-                {
+        parent[root->val] = root;
+        vector<TreeNode*> leaves;
+        while (!que.empty()) {
+            int size = que.size();
+            leaves = vector<TreeNode*>();
+            while (size--) {
+                TreeNode* out = que.front();    que.pop();
+                leaves.push_back(out);
+                if (out->left != NULL) {
+                    parent[out->left->val] = out;
                     que.push(out->left);
-                    parent[out->left] = out;
                 }
-                if (out->right != NULL) 
-                {
+                if (out->right != NULL) {
+                    parent[out->right->val] = out;
                     que.push(out->right);
-                    parent[out->right] = out;
                 }
             }
         }
         
-        // Find common ancestor
-        unordered_set<TreeNode*> s;
-        for (auto node : lastLayer)    s.insert(node);
+        // Find the cloest common ancestor
         while (1) {
-            unordered_set<TreeNode*> temp;
-            for (auto node : s) {
-                temp.insert(parent[node]);
+            if (sameNode(leaves))    return leaves[0];
+            for (int i = 0; i < leaves.size(); i++) {
+                leaves[i] = parent[leaves[i]->val];
             }
-            if (s.size() == 1)    break;
-            s.clear();    s = temp;
         }
         
-        return *(s.begin());
+        return NULL;
+    }
+    
+private:
+    bool sameNode(vector<TreeNode*>& leaves) {
+        int pivot = leaves[0]->val;
+        for (TreeNode* l : leaves) {
+            if (l->val != pivot)    return 0;
+        }
+        return 1;
     }
 };
