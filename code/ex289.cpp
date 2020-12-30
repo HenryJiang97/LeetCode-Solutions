339b0c387a0e92b5d25bbe331fbb401406c3fb56
Henry Jiang <henryjiang97@gmail.com>
2020-12-30 13:04:11 -0800

    Updated ex289.cpp

--- code/ex289.cpp
+++ code/ex289.cpp
@@ -1,33 +1,28 @@
-// 0 => dead
-// 1 => alive
-// 2 => dead -> alive
-// 3 => alive -> dead
-
-const int dir[8][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+// 2: dead->live
+// 3: live->dead
 
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
+        const int dir[8][2] = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
         int m = board.size(), n = board[0].size();
-        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int alive = 0;
-                
+                int live = 0;
                 for (auto& d : dir) {
                     int ni = i + d[0], nj = j + d[1];
                     if (ni < 0 || nj < 0 || ni >= m || nj >= n)    continue;
-                    if (board[ni][nj] == 1 || board[ni][nj] == 3)    alive++;
+                    if (board[ni][nj] == 1 || board[ni][nj] == 3)    live++;
                 }
-                
-                if (board[i][j] == 1 && alive < 2)    board[i][j] = 3;
-                if (board[i][j] == 1 && alive > 3)    board[i][j] = 3;
-                if (board[i][j] == 0 && alive == 3)    board[i][j] = 2;
+                if (board[i][j] == 1 && (live < 2 || live > 3))    board[i][j] = 3;
+                if (board[i][j] == 0 && live == 3)    board[i][j] = 2;
             }
         }
         
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                board[i][j] = board[i][j] == 0 || board[i][j] == 3 ? 0 : 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = board[i][j] == 2 || board[i][j] == 1 ? 1 : 0;
+            }
+        }
     }
 };
--- code/ex435.cpp
+++ code/ex435.cpp
@@ -1,26 +1,18 @@
-// Greedy
-
-struct cmp{
-    bool operator()(vector<int>& a, vector<int>& b) {
-        if (a[0] != b[0])    return a[1] < b[1];
-        else    return a[0] > b[0];
-    }
-};
-
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        int n = intervals.size();    if (n == 0)    return 0;
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
+            return a[1] != b[1] ? a[1] < b[1] : a[1] - a[0] < b[1] - b[0];
+        });
         
-        // Sort the array by the end point in ascending order
-        sort(intervals.begin(), intervals.end(), cmp());
-        
-        int overlap = 0, last_end = intervals[0][1];
-        for (int i = 1; i < n; i++) {
-            if (intervals[i][0] < last_end)    overlap++;
-            else    last_end = intervals[i][1];
+        int fit = 0, end = INT_MIN;
+        for (auto& interval : intervals) {
+            if (interval[0] >= end) {
+                end = max(end, interval[1]);
+                fit++;
+            }
         }
         
-        return overlap;
+        return intervals.size() - fit;
     }
 };
