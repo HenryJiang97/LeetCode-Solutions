54baeba40f9b8fb14ea9d0301799878f53ba0d64
Henry Jiang <henryjiang97@gmail.com>
2021-01-03 15:32:27 -0800

    Added ex199.cpp

--- code/ex199.cpp
+++ code/ex199.cpp
@@ -4,37 +4,27 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         if (root == NULL)    return {};
-        
         vector<int> res;
         queue<TreeNode*> que;
         que.push(root);
-        res.push_back(root->val);
-        
         while (!que.empty()) {
             int size = que.size();
-            TreeNode* rightMost = NULL;
-            for (int z = size; z > 0; z--) {
+            while (size--) {
                 TreeNode* out = que.front();    que.pop();
-                if (out->left != NULL) {
-                    que.push(out->left);
-                    rightMost = out->left;
-                }
-                if (out->right != NULL) {
-                    que.push(out->right);
-                    rightMost = out->right;
-                }
+                if (size == 0)    res.push_back(out->val);
+                if (out->left != NULL)    que.push(out->left);
+                if (out->right != NULL)    que.push(out->right);
             }
-            if (rightMost != NULL)
-                res.push_back(rightMost->val);
         }
-        
         return res;
     }
 };
